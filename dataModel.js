d984e1c90f6838ba65b57fcd1491952bf239c233
Diana Atzin Putnam <98844945+DianaAtzin@users.noreply.github.com>
2024-09-05 17:33:34 -0600

    Add initial todo data model

---
+++ dataModel.js
@@ -0,0 +1,8 @@
+// Variables to represent todo data
+const todoId = 1; // Number type for todo ID
+const todoTitle = "Buy groceries"; // String type for todo title
+const todoDescription = "Milk, eggs, bread"; // String type for todo description
+const todoDueDate = new Date("2024-9-5"); // Date type for todo due date
+const todoPriority = "High"; // String type for todo priority
+const todoCompleted = false; // Boolean type for todo completion status
+const todoCategory = "Grocery"; // String type for todo category
