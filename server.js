6bc92409f7036fa8e60e99ec3710e99067bcf7e0
Diana Atzin Putnam <98844945+DianaAtzin@users.noreply.github.com>
2024-10-24 11:37:28 -0600

    Refactor server.js and add endpoint for retrieving todos

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World Diana!')
-})
+    res.send('Hello to Dianas Todo App!')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//Start here 
+
+let todos = [];
+let categories = [];
+
+app.get('/todos', (req, res) => {   
+    res.json(todos);
+}
+);
+
